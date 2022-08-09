fa17a3243808c7a34ecf2b40452fe87661379167
AI.chemist <108244842+veryberry030@users.noreply.github.com>
2022-08-09 23:16:06 +0900

    author field 추가하기

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
-from distutils.command.upload import upload
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 class Post(models.Model):
@@ -11,9 +11,11 @@ class Post(models.Model):
     
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
     
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
