89cdcc1e861ce5a2a2a4679d001c9f77ece0fd98
veryberry030 <junghyun320@naver.com>
2022-08-11 18:07:06 +0900

    TAG PAGE 만들기

--- dj/settings.py
+++ dj/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from telnetlib import AUTHENTICATION
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,6 +27,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 
     'blog',
     'single_pages',
@@ -117,3 +125,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'_media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'none'
--- dj/urls.py
+++ dj/urls.py
@@ -6,8 +6,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('blog/', include('blog.urls')),
-    path('', include('single_pages.urls')),
     path('admin/', admin.site.urls),
+    path('markdownx/',include('markdownx.urls')),
+    path('accounts/',include('allauth.urls')),
+    path('', include('single_pages.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
